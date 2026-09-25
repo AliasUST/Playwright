0389efbc1cbf58fb0a3dba60b1ad94ae5450902f
Alias <107686012+AliasUST@users.noreply.github.com>
2026-09-25 12:55:22 +0530

    Add page.pause() before login page checks

--- tests/saucedemo-opened-page/login.spec.ts
+++ tests/saucedemo-opened-page/login.spec.ts
@@ -8,7 +8,7 @@ test.describe('Standard user login', () => {
 
   test.beforeEach(async ({ page }) => {
     const login = new LoginPage(page);
-
+    page.pause();
     await login.goto();
     await expect(login.usernameInput).toBeVisible().todo;
     await expect(login.passwordInput).toBeVisible();
